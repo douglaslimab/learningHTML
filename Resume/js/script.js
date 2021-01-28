b7f9ea2e5d7da2cd643ba48024ea188ac11d9195
Douglas Lima Benevides <51888076+douglaslimab@users.noreply.github.com>
2021-01-28 20:57:05 +0000

    uploading images

--- Resume/js/script.js
+++ Resume/js/script.js
@@ -14,10 +14,10 @@ const reactBar = document.querySelector('.bar-react')
 
 var t1 = new TimelineLite()
 
-t1.fromTo(htmlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`}, ease: Power4.easeOut)
-    .fromTo(cssBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`}, ease: Power4.easeOut)
-    .fromTo(jsBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(75% - 6px)`}, ease: Power4.easeOut)
-    .fromTo(reactBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(70% - 6px)`}, ease: Power4.easeOut)
+t1.fromTo(htmlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
+    .fromTo(cssBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
+    .fromTo(jsBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(75% - 6px)`, ease: Power4.easeOut})
+    .fromTo(reactBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(70% - 6px)`, ease: Power4.easeOut})
 
 
 const controller = new ScrollMagic.controller()
